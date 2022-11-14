48829374f9617a7a098313b62356b8da53c5d89b
Raze Kan <ruzo.abdraxmanov@mail.ru>
2022-11-14 20:35:59 +0600

    Dir breach commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>Lorem ipsum dolor.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div>App</div>
+        <div>Dir breach commit 1</div>
+      </div>
   );
 }
 
